531b59f0997fc5bbb01a7eb1fe597e14d7e91248
Ayon <ayonjodder177@gmail.com>
2022-06-16 11:16:53 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#F86338",
+          secondary: "#11142D",
+          accent: "#F4F7F8",
+          // neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
